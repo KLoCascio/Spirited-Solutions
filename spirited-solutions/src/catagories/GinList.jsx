01dfc9b45b7973416fb9d7a1c16876b2ac14a321
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-11-01 11:45:00 -0400

    added files and folders

---
+++ spirited-solutions/src/catagories/GinList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GinList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GinList
---
+++ spirited-solutions/src/details/RumDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RumDetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RumDetails
