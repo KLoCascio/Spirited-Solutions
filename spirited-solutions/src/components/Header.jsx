3c8b7e5cd46332448e68247918f6c986e204c54e
KLoCascio <KyndalLife@gmail.com>
2023-11-01 16:25:00 -0400

    different banners based on location

--- spirited-solutions/src/components/Header.jsx
+++ spirited-solutions/src/components/Header.jsx
@@ -1,13 +1,51 @@
+import Nav from "./Nav"
+import { useLocation } from "react-router-dom"
 
-const Header = () => {
+const Header = (props) => {
+    const location = useLocation()
+
+    let bgClass = 'Bg-Default'
+
+if (location.pathname === '/brandy') {
+    bgClass = 'Bg-Brandy'
+  } else if (location.pathname === '/gin') {
+    bgClass = 'Bg-Gin'
+  } else if (location.pathname === '/rum') {
+    bgClass = 'Bg-Rum'
+  } else if (location.pathname === '/tequila') {
+    bgClass = 'Bg-Tequila'
+  } else if (location.pathname === '/vodka') {
+    bgClass = 'Bg-Vodka'
+  } else if (location.pathname.startsWith('/whiskey')) {
+    bgClass = 'Bg-Whiskey'
+  }
 
     return (
-        <div className="Header">
-            <h1> Spirited Solutions </h1>
-            {/* <img src="./src/assets/IndexBanner.jpg" alt="banner" /> */}
-        </div>
+        <>
+            <div className={`Header ${bgClass}`}>
+                <h1> Spirited Solutions </h1>
+            </div>
+
+            <Nav />
+        </>
     )
 
 }
 
 export default Header
+
+let bgClass = 'Bg-Default'
+
+if (location.pathname === '/brandy') {
+    bgClass = 'Bg-Brandy'
+  } else if (location.pathname === '/gin') {
+    bgClass = 'Bg-Gin'
+  } else if (location.pathname === '/rum') {
+    bgClass = 'Bg-Rum'
+  } else if (location.pathname === '/tequila') {
+    bgClass = 'Bg-Tequila'
+  } else if (location.pathname === '/vodka') {
+    bgClass = 'Bg-Vodka'
+  } else if (location.pathname.startsWith('/whiskey')) {
+    bgClass = 'Bg-Whiskey'
+  }
