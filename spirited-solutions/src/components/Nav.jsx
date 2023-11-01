c66a3cdb09ad85b47aec8ef6ea51aa8377107dfd
KLoCascio <KyndalLife@gmail.com>
2023-11-01 13:20:08 -0400

    fixed?

--- spirited-solutions/src/components/Nav.jsx
+++ spirited-solutions/src/components/Nav.jsx
@@ -1,11 +1,19 @@
-
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
-    
+
     return (
-        <h1>Nav</h1>
+        <div className="Navbar">
+            {/* <button><Link to="/">Home</Link> </button>
+            <button><Link to=""> Brandy</Link></button>
+            <button><Link to=""> Gin</Link></button>
+            <button><Link to=""> Rum</Link></button>
+            <button><Link to=""> Tequila</Link></button>
+            <button><Link to=""> Vodka</Link></button>
+            <button><Link to=""> Whiskey</Link></button> */}
+        </div>
     )
-    
+
 }
 
 export default Nav
