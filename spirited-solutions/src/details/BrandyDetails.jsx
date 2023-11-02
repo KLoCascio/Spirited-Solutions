8c9cf2146a8a37ea894fef8501653c96ecc8473c
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-11-01 21:19:58 -0400

    fix

--- spirited-solutions/src/details/BrandyDetails.jsx
+++ spirited-solutions/src/details/BrandyDetails.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
-import { BASE_URL } from '../globals'
+// import { BASE_URL } from '../globals'
 
 const BrandyDetails = () => {
   let { id } = useParams()
   const [drink, setDrink] = useState(null)
+  console.log(drink)
 
   useEffect(() => {
     const getDrinks = async () => {
     try {
-      const response = await axios.get(`${ BASE_URL }brandy/${id}`)
-      setDrink(response.data)
+      const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+      const drinkData = response.data.drinks[i]
+      console.log(response)
+      setDrink(drinkData)
     } catch (error) {
       console.error(error)
     }
