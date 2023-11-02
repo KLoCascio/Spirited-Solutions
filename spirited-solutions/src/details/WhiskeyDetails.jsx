60b80783a41b36d4eda15fd7986e5d06d16e5839
KLoCascio <KyndalLife@gmail.com>
2023-11-01 23:36:50 -0400

    classes

--- spirited-solutions/src/details/WhiskeyDetails.jsx
+++ spirited-solutions/src/details/WhiskeyDetails.jsx
@@ -1,38 +1,37 @@
-import { useEffect, useState } from "react"
-import { useParams, Link } from 'react-router-dom'
-import axios from 'axios'
-import { BASE_URL } from '../globals'
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../globals';
 
 export default function WhiskeyPage() {
-
-    const [whiskey, setWhiskey] = useState(null)
-    const { id } = useParams()
+    const [whiskey, setWhiskey] = useState(null);
+    const { id } = useParams();
 
     useEffect(() => {
         const getWhiskeyDetails = async () => {
             try {
-                const response = await axios.get(`${BASE_URL}lookup.php?i=${id}`)
-                setWhiskey(response.data.drinks[0])
+                const response = await axios.get(`${BASE_URL}lookup.php?i=${id}`);
+                setWhiskey(response.data.drinks[0]);
             } catch (error) {
                 console.error("Error fetching whiskey details:", error);
-                setWhiskey(null)
-        }
-    }
-    getWhiskeyDetails()
-    }, [id])
+                setWhiskey(null);
+            }
+        };
+        getWhiskeyDetails();
+    }, [id]);
 
     return whiskey ? (
         <div className="details">
-\
-                <p><img src={whiskey.strDrinkThumb}></img></p>
-                <p>{whiskey.strDrink}</p>
-                <p>{whiskey.strMeasure1} {whiskey.strWhiskey1}</p>
-                <p>{whiskey.strMeasure2} {whiskey.strWhiskey2}</p>
-                <p>{whiskey.strMeasure3} {whiskey.strWhiskey3}</p>
+            <img src={whiskey.strDrinkThumb} alt={whiskey.strDrink} className="drink-image" />
+            <h1 className="drink-title">{whiskey.strDrink}</h1>
+            <p className="drink-info">{whiskey.strIngredient1} {whiskey.strMeasure1}</p>
+            <p className="drink-info">{whiskey.strIngredient2} {whiskey.strMeasure2}</p>
+            <p className="drink-info">{whiskey.strIngredient3} {whiskey.strMeasure3}</p>
+            <p className="drink-info">{whiskey.strIngredient4} {whiskey.strMeasure4}</p>
+            <p className="drink-info">{whiskey.strIngredient5} {whiskey.strMeasure5}</p>
 
-            <h4><span>Instructions:</span><br/>{whiskey.strInstructions}</h4>
-            <Link to="/WhiskeyList">Return To Whiskey</Link>
+            <h4 className="drink-instructions">Instructions:<br/>{whiskey.strInstructions}</h4>
+            <Link to="/WhiskeyList" className="return-link">Return To Whiskey</Link>
         </div>
-    ) : <h2 className="Finding">Loading Drink...</h2>
-
+    ) : <h2 className="Finding">Loading Drink...</h2>;
 }
